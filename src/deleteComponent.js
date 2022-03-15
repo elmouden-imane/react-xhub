6b34bdb43b68bf2be8bd46a66f9d9e773db34e8e
elmouden-imane <elmouden.imane@ensam-casa.ma>
2022-03-15 13:43:20 +0100

    delete

---
+++ src/deleteComponent.js
@@ -0,0 +1,10 @@
+
+const Delete = ({handleClick}) => {
+    return (
+      <div className="Delete">
+      <button onClick={()=>handleClick()}>Delete </button>
+      </div>
+    );
+  }
+   
+  export default Delete;
--- src/todoList.js
+++ src/todoList.js
@@ -1,16 +1,21 @@
 import { useState } from "react";
 
 
-const TodoList = ({listData}) => {
+const TodoList = ({listData , handleCheckbox}) => {
     // const [nb,SetNB] = useState(1)
+
     return (
       <div>
           <ul>
               {/* <li>{nb}</li> */}
               {listData.map(
-                item=>
-                <li key={item}>{item}</li>
+                (item)=>
+                <><li id={item.id} key={item}> {item.label} <input id={item.id} type="checkbox"
+                checked={item.value}
+                onChange={()=>handleCheckbox(item.id)}  />  
+                </li></>
                 )}
+
           </ul>
       </div>
     );
